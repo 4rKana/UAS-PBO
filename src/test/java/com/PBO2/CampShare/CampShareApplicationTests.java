79d2fdedb4f6a152728f5e00f8cbde695396e2f7
ArKana <aristria91@gmail.com>
2026-06-13 22:38:33 +0700

    first commit: reset total base project

---
+++ src/test/java/com/PBO2/CampShare/CampShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PBO2.CampShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
